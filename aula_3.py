e0bdd28daff4d3b0b3dd17a6db55e9e34881a351
unknown <mayconfontana@gmail.com>
2019-11-16 11:49:29 -0300

    Subindo as primeiras aulas

---
+++ aula_3.py
@@ -0,0 +1,196 @@
+#x = 3
+#y = 5
+
+#if x > y:
+#	print('sim')
+#print('saiu do if')
+
+#nome = 'gabriela'
+#if 'bru' in nome:
+#	print('sim')
+
+#lista_frutas = ['maca','banana','laranja']
+
+#if 'banana' in lista_frutas:
+#	print('tem banana no RU')
+
+
+#x = 10
+#y = 20
+
+#if x > y:
+#	print('x é maior que y')
+#else:
+#	print('y é maior que x')
+
+'''x = -1
+
+if x > 0:
+	print('x é positivo')
+if x == 0:
+	print('x é zero!')
+else:
+	print('x é negativo')
+
+
+print('passou')'''
+
+'''x = 0
+
+while x < 5:
+	print('x')
+	x = x + 1
+
+print('passou')'''
+
+
+'''x = 0
+
+while x < 5:
+	x = x + 1
+	if x == 2:
+		continue
+	print(x)
+
+
+print('passou')'''
+
+#alunos = ['caio', 'pedro', 'vittor', 'gabriela']
+#aluna = 'gabriela'
+
+#for i in range(0,1,100):
+#	print(i)
+
+
+# Exercicio Soma de Gauss
+
+'''soma = 0
+numero = 1
+
+while numero <= 100:
+	soma = soma + numero
+	numero = numero + 1
+print(soma)'''
+
+'''l1 = 5
+l2 = 6
+l3 = 4
+
+# Verificando se um dos lados é zero
+
+if l1 == 0 or l2 == 0 or l3 == 0:
+	print('não é um triangulo: pelo menos um dos lados é zero')
+	quit()
+
+# Verificando se um lado é menor ou menor que a soma dos outros dois
+
+if l1>=l2+l3 or l2>=l1+l3 or l3>=l1+l2:
+	print('não é um triangulo: a media de pelo menos um dos lados é menor que a soma dos outros lados')
+
+if l1 == l2 == l3:
+	print('É um triangulo equilatero')
+elif l1 == l2 or l1 == l3 or l2 == l3: 
+	print('É um triangulo isoceles')
+else:
+	print('É um triangulo escaleno')'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ aula_4.py
@@ -0,0 +1,43 @@
+
+alunos = ['laio melo', 'vittor', 'wgabriela', 'pedro']
+
+# print('vittor valler' not in alunos)
+
+novo_aluno = ['atrasado']
+
+alunos_final = novo_aluno + alunos  
+
+duplicada = alunos_final * 2
+
+#print(duplicada)
+
+#print(len(alunos_final))
+
+notas = [2,3,8,9,3,2,10]
+
+#print(max(notas))
+
+
+serie_historica = [[2,3,1],[3,5,6],[7,8,9],[10,11,12],[23,45,68]]
+
+primeiro_ano = serie_historica[0][1]
+
+#print(primeiro_ano)
+
+#print(type(primeiro_ano))
+
+x = ['a',['bb',['ccc','ddd'],'ee','ff'],'g',['hh','ii'],'j']
+
+print(x[1])
+
+lista_vazia = []
+
+
+
+lista_vazia.append(1) 
+
+print(lista_vazia)
+
+
+
+
