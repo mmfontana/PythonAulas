06485052b732c7a3003ab5fcb8dc31c747b4c297
mmfontana <mayconfontana@gmail.com>
2020-05-12 21:09:55 -0300

    subindo segunda versao do projeto final

--- projeto_final/processamento_corona.py
+++ projeto_final/processamento_corona.py
@@ -1,11 +1,37 @@
-from raspagem_corona import RaspagemCorona
+from raspagem_corona import RaspagemCorona, TratamentoDados
+
+
 
 if __name__ == '__main__':
 
 	raspador = RaspagemCorona()
 
-	# for
-	# 	for
+	paises_out = [5, 6, 20]
+	n_primeiro = 4 
+	n_ultimo_pais = 34
 
-		numero = raspador.RasparDados()
-		raspador.ParaDicionario(numero)
+	dict_final = dict()
+
+	for pais in range(n_primeiro, n_ultimo_pais): 
+		
+		dados_pais = list()
+
+		if pais not in paises_out:
+
+			for coluna in range(1,13):
+				dado = raspador.RasparDados(pais, coluna)
+				dado = raspador.TratarString(dado)
+				dados_pais.append(dado)
+
+			dict_final = raspador.ParaDicionario(dados_pais, dict_final)
+
+		else:
+
+			pass
+
+	tratamento = TratamentoDados()
+
+	df = tratamento.ParaDataFrame(dict_final)
+	tratamento.ParaExcel(df)
+		
+print(df)
--- projeto_final/raspagem_corona.py
+++ projeto_final/raspagem_corona.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import pandas as pd
 
 class RaspagemCorona():
 
@@ -7,20 +8,57 @@ class RaspagemCorona():
 		self.driver = webdriver.Chrome()
 		self.driver.get('https://www.worldometers.info/coronavirus/')
 
-	def RasparDados(self):
+	def RasparDados(self, pais, coluna):
 
+		dado = self.driver.find_element_by_xpath('//*[@id="main_table_countries_today"]/tbody[1]/tr[{}]/td[{}]'.format(pais,coluna)).text
+		
+		return dado		
 
-		for i in range(4,41): # Ativiade: levar os dois for para fora 
-			for j in range(1,13):
-					
+	def ParaDicionario(self, dados_pais, dict_final):
 
-				data = self.driver.find_element_by_xpath('//*[@id="main_table_countries_today"]/tbody[1]/tr[{}]/td[{}]'.format(i,j)).text
-				lista_data.append(data)
-				print(type(data))
-	
-	
-	# def ParaDicionario():
+		dict_final[dados_pais[0]] = dict()
 
-		# dicionario_data = {'eua': {numero_mortes: 10000, numero_casos: 20000}, 'italy': {numero_mortes: 10000, numero_casos: 20000}}	 
-	
+		dict_final[dados_pais[0]]['Total de Casos'] = dados_pais[1]
+		dict_final[dados_pais[0]]['Novos Casos'] = dados_pais[2]
+		dict_final[dados_pais[0]]['Total de Mortes'] = dados_pais[3]
+		dict_final[dados_pais[0]]['Novas Mortes'] = dados_pais[4]
+		dict_final[dados_pais[0]]['Total de Recuperados'] = dados_pais[5]
+		dict_final[dados_pais[0]]['Casos Ativos'] = dados_pais[6]
+		dict_final[dados_pais[0]]['Sério ou Crítico'] = dados_pais[7]
+		dict_final[dados_pais[0]]['Total de Casos/1M População'] = dados_pais[8]
+		dict_final[dados_pais[0]]['Mortes/1M População'] = dados_pais[9]
+		dict_final[dados_pais[0]]['Total de Testes'] = dados_pais[10]
+		dict_final[dados_pais[0]]['Testes/1M População'] = dados_pais[11]
+
+		return dict_final
+
+	def TratarString(self, dado):
+
+		if ',' in dado:
+			dado = dado.replace(',', '')
+
+		if '+' in dado:
+			dado = dado.replace('+', '') 
+
+		if dado == '':
+			dado = 0
+		
+		try:
+			dado = int(dado)
+		except:	
+			pass
+
+		return dado
+
+class TratamentoDados():
+
+	def ParaDataFrame(self, dict_final):
+
+		df = pd.DataFrame(dict_final).T
+
+		return df
+
+	def ParaExcel(self, df):
+
+		df.to_excel('out_corona.xlsx', sheet_name = 'analise_corona')
 
